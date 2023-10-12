fa09fc78731f271c0cd1eab8069b958551cfc1d1
heesae <134567479+h2s0@users.noreply.github.com>
2023-10-12 15:53:11 +0900

    [level unrated] Title: 배열 원소의 길이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,7 +1,7 @@
 function solution(strlist) {
     let answer = [];
-    for( let str of strlist ) {
-        answer.push(str.length);
-    }
+    strlist.map((item) => {
+        answer.push(item.length);
+    });
     return answer;
 }
