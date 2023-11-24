89f4f8540e332a3b7c76252c28dc4c13a16698a7
heesae <134567479+h2s0@users.noreply.github.com>
2023-11-24 13:43:50 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -0,0 +1,8 @@
+function solution(num_list) {
+    let multi = 1;
+    let plus = 0;
+    num_list.forEach((num) => multi *= num)
+    num_list.forEach((num) => plus += num)
+    let plus2 = plus * plus
+    return multi < plus2 ? 1 : 0
+}
