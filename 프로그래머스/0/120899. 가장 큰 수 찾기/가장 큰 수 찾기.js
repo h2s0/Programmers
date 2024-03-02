2f3b0baf1126eecccca566f873812a76585966e5
heesae <134567479+h2s0@users.noreply.github.com>
2024-03-02 22:17:07 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,4 +1,7 @@
 function solution(array) {
     let big = Math.max(...array)
     return [big, array.indexOf(big)]
+    
+    // 짧게하기
+    // return [Math.max(...array), array.indexOf(Math.max(...array))]
 }
