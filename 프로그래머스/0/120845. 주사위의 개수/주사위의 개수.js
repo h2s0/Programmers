058a0a37ee96c27f3f90d9ab97df84f4e5df3ae3
heesae <134567479+h2s0@users.noreply.github.com>
2024-09-29 17:23:04 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,7 +1,11 @@
 function solution(box, n) {
-    let arr = [];
-    box.map((i) => {
-        arr.push(Math.floor(i / n));
-    })
-    return arr.reduce((acc, cur) => acc * cur)
+    // let arr = [];
+    // box.map((i) => {
+    //     arr.push(Math.floor(i / n));
+    // })
+    // return arr.reduce((acc, cur) => acc * cur)
+    
+    // 구조분해할당
+    let [width, length, height] = box;
+    return Math.floor(width/n) * Math.floor(length/n) * Math.floor(height/n);
 }
